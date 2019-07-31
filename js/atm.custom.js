ad0d95523db729311cc5c3dd1ed9b1b7084ad4bb
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-07-30 20:23:12 -0500

    testing data checks and alert calls

--- js/atm.custom.js
+++ js/atm.custom.js
@@ -2,6 +2,11 @@ var balance = 500.00;
 var checkBalance = true;
 var isActive = false;
 
+var testNum = parseFloat(prompt("test: enter a number"));
+if (isNaN(testNum) || testNum === '') {
+    alert("Please enter a real number thank you very much");
+}
+
 if (!checkBalance) {
   console.log("Thank you. Have a nice day");
 } else if (isActive && balance > 0){
