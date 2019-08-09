ce4815786fc5fc82cd91474e79efd880acf43be6
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-08-09 13:49:57 -0500

    finished the basic ATM functionality

--- js/atm.custom.js
+++ js/atm.custom.js
@@ -1,6 +1,7 @@
 var balance = 500.00;
-var checkBalance = true;
-var isActive = false;
+// var checkBalance = true;
+// var isActive = true;
+let exitATM = false;
 
 function makeDeposit() {
     var deposit = parseFloat(prompt("Please enter your deposit"));
@@ -9,29 +10,52 @@ function makeDeposit() {
         makeDeposit();
     }
     balance += deposit;
+    atm();
 }
 
 function makeWithdrawl() {
     var withdrawl = parseFloat(prompt("How much would you like to withdraw?"));
-    if (isNaN(withdrawl) || deposit === '') {
+    if (isNaN(withdrawl) || withdrawl === '') {
         alert("Please enter a real number.");
         makeWithdrawl();
     }
     balance -= withdrawl;
+    atm();
 }
 
-function checkBalance() {
-    if (!checkBalance) {
-        console.log("Thank you. Have a nice day");
-    } else if (isActive) {
-        alert("Current Balance is " + balance.toFixed(2));
+// It was not being viewed as a function when I named it checkBalance(), but this is fine...
+function viewBalance() {
+    alert("Current Balance is " + balance.toFixed(2));
+    atm();
+}
+
+// TODO finish this function
+function exitTransaction() {
+    exitATM = true;
+    if (exitATM) {
+        window.close();
+    } else {
+        alert("Improper exit attempted. Returning to main menu.");
+        atm();
     }
 }
 
 // I don't have a system to use previously-created booleans. Gonna have to keep this simple
 function atm() {
     var atmInteraction = parseFloat(prompt(
-        "Please choose between:\n1. Check Balance\n2. Deposit\n3. Withdrawl\n(Please input a number associated with your desired action"));
+        "Please choose between:\n1. Check Balance\n2. Deposit\n3. Withdrawl\n4. Exit\n(Please input a number associated with your desired action"));
+    if (atmInteraction < 1 || atmInteraction > 4) {
+        alert("Please select a valid option.");
+        atm();
+    } else if (atmInteraction === 1) {
+        viewBalance();
+    } else if (atmInteraction === 2) {
+        makeDeposit();
+    } else if (atmInteraction === 3) {
+        makeWithdrawl();
+    } else {
+        exitTransaction();
+    }
 }
 // var bool = 'Depot' = 'depot';
 // console.log(bool);
@@ -39,14 +63,14 @@ function atm() {
 atm();
 
 // Original
-if (!checkBalance) {
-  console.log("Thank you. Have a nice day");
-} else if (isActive && balance > 0){
-    console.log("Your balance is $" + balance.toFixed(2) + ".");
-} else if (!isActive) {
-    console.log("Your account is no longer active.");
-} else if (balance === 0){
-    console.log("Your account is empty.");
-} else {
-    console.log("Your balance is negative. Please contact bank.");
-}
+// if (!checkBalance) {
+//   console.log("Thank you. Have a nice day");
+// } else if (isActive && balance > 0){
+//     console.log("Your balance is $" + balance.toFixed(2) + ".");
+// } else if (!isActive) {
+//     console.log("Your account is no longer active.");
+// } else if (balance === 0){
+//     console.log("Your account is empty.");
+// } else {
+//     console.log("Your balance is negative. Please contact bank.");
+// }
